dcea0a625e06242631b30b341e3e4287110456e9
EDWINCSN <c.edwin32@hotmail.com>
2026-05-14 00:55:33 -0500

    EGCA: se agrega la clase LeerContacto, con control de errores y excepciones

---
+++ src/main/java/com/krakedev/taller/excepciones/LeerContacto.java
@@ -0,0 +1,42 @@
+package com.krakedev.taller.excepciones;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LeerContacto {
+	
+	private static final Logger log = LoggerFactory.getLogger(LeerContacto.class);
+	
+	public static void main(String[] args) {
+		FileReader lector = null;
+		BufferedReader leerlinea=null;
+		
+		try {
+			lector=new FileReader("contactos.txt");
+			leerlinea=new BufferedReader(lector);
+			String linea="";
+			for(linea=leerlinea.readLine();linea!=null;linea=leerlinea.readLine()) {
+				log.info(linea);
+			}			
+		} catch (FileNotFoundException e) {
+			log.error("Archivo contactos no ecnontrado: "+e);
+		} catch (IOException e) {
+			
+			log.error("Error al realizar lectura de linea por linea "+e);
+		}finally {
+			try {
+				leerlinea.close();
+			} catch (IOException e) {
+				log.error("Error al cerrar leerLinea BufferedReader " + e);
+			}
+		}
+	
+
+	}
+
+}
