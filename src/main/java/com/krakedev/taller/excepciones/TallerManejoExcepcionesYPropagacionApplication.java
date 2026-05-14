cbf4c7f022eab9fdd0782f0ce4fb536c97d40c73
EDWINCSN <c.edwin32@hotmail.com>
2026-05-14 00:23:49 -0500

    EGCA: incializar proyecto

---
+++ src/main/java/com/krakedev/taller/excepciones/TallerManejoExcepcionesYPropagacionApplication.java
@@ -0,0 +1,13 @@
+package com.krakedev.taller.excepciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerManejoExcepcionesYPropagacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerManejoExcepcionesYPropagacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krakedev/taller/excepciones/TallerManejoExcepcionesYPropagacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakedev.taller.excepciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerManejoExcepcionesYPropagacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
